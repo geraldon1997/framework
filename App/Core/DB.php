1a808b6f7793c1cc53291f0f3923470e75af6b90
geraldon1997 <geraldon1997@gmail.com>
2020-05-13 12:42:59 -0400

    building the sql queries

--- App/Core/DB.php
+++ App/Core/DB.php
@@ -4,7 +4,7 @@ namespace App\Core;
 use mysqli;
 use App\Core\Config;
 
-class DbConnection
+class Db
 {
     public static $host;
     public static $user;
--- App/Core/Gateway.php
+++ App/Core/Gateway.php
@@ -5,73 +5,73 @@ use App\Core\DB;
 
 class Gateway extends DB
 {
-  public static function create()
-  {
-    DB::init()->query($sql);
-  }
-
-  public static function insert()
-  {
-    $sql = "INSERT INTO $table ($cols) VALUES ($vals)";
-    DB::init()->query($sql);
-  }
-
-  public static function all()
-  {
-	$sql = "SELECT * FROM $table"
-  }
-
-  public static function find()
-  {
-    $sql = "SELECT * FROM $table WHERE $col = '$col'";
-  }
-
-  public static function check()
-  {
-    $sql = "SELECT * FROM $table WHERE $col = '$val' LIMIT 1";
-  }
-
-  public static function allAsc()
-  {
-    $sql = "SELECT * FROM $table ORDER BY $col ASC";
-  }
-
-  public static function allDesc()
-  {
-    $sql = "SELECT * FROM $table ORDER BY $col DESC";
-  }
-
-  public static function paginate()
-  {
-    $sql = "SELECT * FROM $table LIMIT $num";
-  }
-
-  public static function paginateAsc()
-  {
-    $sql = "SELECT * FROM $table ORDER BY $col ASC LIMIT $num";
-  }
-
-  public static function paginateDesc()
-  {
-    $sql = "SELECT * FROM $table ORDER BY $col DESC LIMIT $num";
-  }
-
-  public static function update()
-  {
-    $sql = "UPDATE $table SET $set WHERE $col = '$val'";
-  }
-
-  public static function getId()
-  {
-    $sql = "SELECT `id` FROM $table WHERE $col = '$val' LIMIT 1";
-  }
-
-  public static function getLastId()
-  {
-    $sql = "SELECT * FROM $table ORDER BY id DESC LIMIT 1";
-    $result = DB::init()->query($sql);
-    foreach ($result as $val) {
-      return $val['id'];
+    public static function create(string $sql)
+    {
+        DB::init()->query($sql);
     }
-  }
+
+    public static function insert(string $table, string $cols, string $vals)
+    {
+        $sql = "INSERT INTO $table ($cols) VALUES ($vals)";
+        DB::init()->query($sql);
+    }
+
+    public static function all(string $table)
+    {
+        $sql = "SELECT * FROM $table";
+    }
+
+    public static function find(string $table, string $col, string $val)
+    {
+        $sql = "SELECT * FROM $table WHERE $col = '$val'";
+    }
+
+    public static function check(string $table, string $col, string $val)
+    {
+        $sql = "SELECT * FROM $table WHERE $col = '$val' LIMIT 1";
+    }
+
+    public static function allAsc(string $table, string $col)
+    {
+        $sql = "SELECT * FROM $table ORDER BY $col ASC";
+    }
+
+    public static function allDesc()
+    {
+        $sql = "SELECT * FROM $table ORDER BY $col DESC";
+    }
+
+    public static function paginate()
+    {
+        $sql = "SELECT * FROM $table LIMIT $num";
+    }
+
+    public static function paginateAsc()
+    {
+        $sql = "SELECT * FROM $table ORDER BY $col ASC LIMIT $num";
+    }
+
+    public static function paginateDesc()
+    {
+        $sql = "SELECT * FROM $table ORDER BY $col DESC LIMIT $num";
+    }
+
+    public static function update()
+    {
+        $sql = "UPDATE $table SET $set WHERE $col = '$val'";
+    }
+
+    public static function getId()
+    {
+        $sql = "SELECT `id` FROM $table WHERE $col = '$val' LIMIT 1";
+    }
+
+    public static function getLastId()
+    {
+        $sql = "SELECT * FROM $table ORDER BY id DESC LIMIT 1";
+        $result = DB::init()->query($sql);
+        foreach ($result as $val) {
+            return $val['id'];
+        }
+    }
 }
